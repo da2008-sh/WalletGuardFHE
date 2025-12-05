dc21ae8cf7e6601eb6d6d11d67baf00b843a5496
da2008-sh <florentc_s_486506451@rambler.ru>
2025-12-05 11:02:13 +0800

    add subscriptions-constants.go, package.json, frontend-shared.adoc

---
+++ frontend/web/src/subscriptions-constants.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\subscriptions-constants.go
+// Generated at: 2025-10-25T14:20:42.204Z
---
+++ payment.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\payment.java
+// Generated at: 2025-10-25T14:20:42.194Z
